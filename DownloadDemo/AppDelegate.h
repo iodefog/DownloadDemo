3c046235dce2ba01d491afa56c43aae11317bd19
lihongli <honglili@sohu-inc.com>
2016-08-19 11:14:08 +0800

    Initial Commit

---
+++ DownloadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DownloadDemo
+//
+//  Created by LHL on 16/8/19.
+//  Copyright © 2016年 lihongli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DownloadDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DownloadDemo
+//
+//  Created by LHL on 16/8/19.
+//  Copyright © 2016年 lihongli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
